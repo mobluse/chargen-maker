d6a8026213bdc1d983753fd2da4b49f09835ee3f
Mikael O. Bonnier <mobluse@users.noreply.github.com>
2019-09-08 23:31:21 +0200

    Update bin2txt.sh

--- bin2txt.sh
+++ bin2txt.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-xxd -b -c 1 chargen.bin | sed 's/.*://;s/0/\\/g;s/1/@/g;s/ //;s/ .*//;0~8G' > chargen-new.txt
+if [ -z $1 ]
+then
+   fn=chargen
+else
+   fn=$1
+fi
+xxd -b -c 1 $fn.bin | sed 's/.*://;s/0/\\/g;s/1/@/g;s/ //;s/ .*//;0~8G' > $fn-new.txt
