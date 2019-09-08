a47e64957d06f665ae8d5365bd21e3f47571afdb
Mikael O. Bonnier <mobluse@users.noreply.github.com>
2019-09-09 00:07:09 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-if [ -z $1 ]
+if [ -z "$1" ]
 then
    fn=chargen
 else
-   fn=$1
+   fn="$1"
 fi
-fn=$(basename -s .txt $fn)
-sed '/^[[:space:]]*$/d;s/\\/0/g;s/@/1/g;' < $fn.txt | perl -ne 'print pack("B8", $_)' > $fn.bin
+fn=$(basename -s .txt "$fn")
+sed '/^[[:space:]]*$/d;s/\\/0/g;s/@/1/g;' < "$fn".txt | perl -ne 'print pack("B8", $_)' > "$fn".bin
