7c5a352e8bf697f34890b8681b45093d00b18c95
Mikael O. Bonnier <mobluse@users.noreply.github.com>
2019-09-08 23:51:55 +0200

    Update bin2txt.sh

--- bin2txt.sh
+++ bin2txt.sh
@@ -3,8 +3,8 @@ if [ -z $1 ]
 then
    fn=chargen
 else
-   fn=$1
+   fn="$1"
 fi
-fn=$(basename -s .bin $fn)
-fn=$(basename -s .rom $fn)
-xxd -b -c 1 $fn.bin | sed 's/.*://;s/0/\\/g;s/1/@/g;s/ //;s/ .*//;0~8G' > $fn-new.txt
+fn=$(basename -s .bin "$fn")
+fn=$(basename -s .rom "$fn")
+xxd -b -c 1 "$fn".bin | sed 's/.*://;s/0/\\/g;s/1/@/g;s/ //;s/ .*//;0~8G' > "$fn"-new.txt
