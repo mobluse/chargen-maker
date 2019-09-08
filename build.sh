fe09890939be8a5ff179a7d9a54142f91dbcb487
Mikael O. Bonnier <mobluse@users.noreply.github.com>
2019-09-08 23:29:17 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ then
 else
    fn=$1
 fi
-sed '/^[[:space:]]*$/d;s/\\/0/g;s/@/1/g;' < $1.txt | perl -ne 'print pack("B8", $_)' > $1.bin
+sed '/^[[:space:]]*$/d;s/\\/0/g;s/@/1/g;' < $fn.txt | perl -ne 'print pack("B8", $_)' > $fn.bin
