8daebe5bcbb3bbfbc7ea483338e4ba020757449b
Mikael O. Bonnier <mobluse@users.noreply.github.com>
2019-09-08 23:23:50 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-sed '/^[[:space:]]*$/d;s/\\/0/g;s/@/1/g;' < chargen.txt | perl -ne 'print pack("B8", $_)' > chargen.bin
+if [-z $1]
+then
+   $1 = chargen
+fi
+sed '/^[[:space:]]*$/d;s/\\/0/g;s/@/1/g;' < $1.txt | perl -ne 'print pack("B8", $_)' > $1.bin
